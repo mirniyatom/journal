24c76c5b6c2d267a81935bee3ef83fc319a75f5d
mirniyatom <nazarmamatov15@gmail.com>
2025-09-06 21:51:33 +0500

    sahiflar tayyor

--- journal/mainmenu/urls.py
+++ journal/mainmenu/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('<main_menu>/', views.get_mainmenu, name='main_menu'),
 ]
--- journal/mainmenu/views.py
+++ journal/mainmenu/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 
@@ -15,6 +15,18 @@ mainmenu_dict = {
     'login': 'Avtorizatsiya yoki registratsiya'
 
 }
+
+def index(request):
+    li_elements = ''
+    for m in mainmenu_dict:
+        redirect_path = reverse('main_menu', args=[m,])
+        li_elements += f'<li> <a href="{redirect_path}">{m.title()}</a></li>'
+    response = f"""
+            <ul style="list-style-type: none; display: flex; gap: 15px; padding: 0; margin: 0;">
+                {li_elements}
+            </ul>"""
+    return HttpResponse(response)
+
 
 def get_mainmenu(request, main_menu):
     desc = mainmenu_dict.get(main_menu)
